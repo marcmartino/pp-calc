f9d4da834a14385ba959eee64491ad0295037b2d
Marc Martino <marc@marmar.io>
2021-06-10 17:36:44 -0600

    calculations might be right? unit tests are def in order

---
+++ src/calculations.ts
@@ -0,0 +1,144 @@
+import {
+  PrestigeMultiplier,
+  PrestigeMultipliersWithValue,
+  PrestigeMultiplierCosts,
+  multiplierIndex,
+  initialPrestigeMultCosts,
+  PrestigePoints,
+  PrestigeLevel,
+  Dollars,
+  moneyPerPrestige,
+  Multiplier,
+} from "./constants";
+
+export const cheapestMultiplierOption = (
+  costs: PrestigeMultipliersWithValue
+): PrestigeMultiplierCosts =>
+  costs.reduce((cheapest, multOption) =>
+    cheapest[1].multPerPrest >= multOption[1].multPerPrest
+      ? cheapest
+      : multOption
+  );
+
+export const incrementMultiplier =
+  (mult: PrestigeMultiplier) =>
+  (costs: PrestigeMultipliersWithValue): PrestigeMultipliersWithValue => {
+    const updatedCosts = [...costs];
+    const prevMultVals = costs[multiplierIndex[mult]];
+    const newCost =
+      prevMultVals[1].cost +
+      initialPrestigeMultCosts[multiplierIndex[mult]][1].cost;
+    updatedCosts[multiplierIndex[mult]] = [
+      prevMultVals[0],
+      {
+        qty: costs[multiplierIndex[mult]][1].qty + 1,
+        cost: newCost,
+        multPerPrest: mult / newCost,
+      },
+    ];
+
+    // TODO: would be nice to avoid the typecast but this should be fine given that mult is valid
+    // one day i'll learn monocle-ts :\
+    return updatedCosts as PrestigeMultipliersWithValue;
+  };
+
+const minPrestigeToGetMult =
+  (wantedMultiplier: Multiplier, prevCost = 0) =>
+  (
+    mults = initialPrestigeMultCosts
+  ): [prestige: PrestigePoints, multipliers: PrestigeMultipliersWithValue] => {
+    if (wantedMultiplier <= 0) return [prevCost, mults];
+    const cheapestMult = cheapestMultiplierOption(mults);
+    return minPrestigeToGetMult(
+      wantedMultiplier - cheapestMult[0],
+      prevCost + cheapestMult[1].cost
+    )(incrementMultiplier(cheapestMult[0])(mults));
+  };
+
+export const maxMultFromPrestigePoints = (
+  points: PrestigePoints,
+  multipliers = initialPrestigeMultCosts
+): [
+  result: { multiplier: Multiplier; remainingPrest: PrestigePoints },
+  multipliers: PrestigeMultipliersWithValue
+] => {
+  const cheapest = multipliers.reduce<PrestigeMultiplierCosts | undefined>(
+    (x, y) =>
+      (x !== undefined && x[1].multPerPrest >= y[1].multPerPrest) ||
+      y[1].cost > points
+        ? x
+        : y,
+    undefined
+  );
+
+  if (cheapest) {
+    const subsequentIteration = maxMultFromPrestigePoints(
+      points - cheapest[1].cost,
+      incrementMultiplier(cheapest[0])(multipliers)
+    );
+    return [
+      {
+        multiplier: cheapest[0] + subsequentIteration[0].multiplier,
+        remainingPrest: subsequentIteration[0].remainingPrest,
+      },
+      subsequentIteration[1],
+    ];
+  }
+  return [{ multiplier: 0, remainingPrest: points }, multipliers];
+};
+
+export const dollarsNeededForMult =
+  (level: PrestigeLevel) =>
+  (
+    wantedMultiplier: number
+  ): [cost: Dollars, multipliers: PrestigeMultipliersWithValue] => {
+    const minPrestige = minPrestigeToGetMult(wantedMultiplier)(
+      initialPrestigeMultCosts
+    );
+    return [moneyPerPrestige[level] * minPrestige[0], minPrestige[1]];
+  };
+
+// TODO: come back to address the perf issues here
+// export const maxMultFromPrestigePoints = (
+//   points: PrestigePoints,
+//   multipliers = initialPrestigeMultCosts
+// ): [
+//   result: { multiplier: Multiplier; remainingPrest: PrestigePoints },
+//   multipliers: PrestigeMultipliersWithValue
+// ] =>
+//   pipe(
+//     multipliers,
+//     A.filter(([_, { cost }]) => points >= cost),
+//     NEA.fromArray,
+//     O.chain((neMultipliers) => {
+//       const initialMult = NEA.head(neMultipliers);
+//       return pipe(
+//         neMultipliers,
+//         NEA.tail,
+//         NEA.fromArray,
+//         O.match(
+//           () => O.some(initialMult),
+//           (otherMults) =>
+//             O.some(
+//               NEA.reduce(initialMult, Ord.max(ordMultiplierCost))(otherMults)
+//             )
+//         )
+//       );
+//     }),
+//     O.match(
+//       () => [{ multiplier: 0, remainingPrest: points }, multipliers],
+//       (cheapestMult) => {
+//         const subsequentIteration = maxMultFromPrestigePoints(
+//           points - cheapestMult[1].cost,
+//           incrementMultiplier(cheapestMult[0])(multipliers)
+//         );
+//         return [
+//           {
+//             multiplier: cheapestMult[0] + subsequentIteration[0].multiplier,
+//             remainingPrest: subsequentIteration[0].remainingPrest,
+//           },
+//           subsequentIteration[1],
+//         ];
+//       }
+//     )
+//   );
---
+++ src/constants/index.ts
@@ -0,0 +1,99 @@
+export const prestigeLevels = [
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
+  42, 43, 44, 45, 46, 47, 48, 49, 50,
+] as const;
+
+export const moneyPerPrestige: Record<PrestigeLevel, Dollars> = {
+  1: 12000,
+  2: 56000,
+  3: 144000,
+  4: 288000,
+  5: 500000,
+  6: 792000,
+  7: 1176000,
+  8: 1664000,
+  9: 2268000,
+  10: 3000000,
+  11: 3872000,
+  12: 4896000,
+  13: 6084000,
+  14: 7448000,
+  15: 9000000,
+  16: 10752000,
+  17: 12716000,
+  18: 14904000,
+  19: 17328000,
+  20: 20000000,
+  21: 22932000,
+  22: 26136000,
+  23: 29624000,
+  24: 33408000,
+  25: 37500000,
+  26: 41912000,
+  27: 46656000,
+  28: 51744000,
+  29: 57188000,
+  30: 63000000,
+  31: 69192000,
+  32: 75776000,
+  33: 82764000,
+  34: 90168000,
+  35: 98000000,
+  36: 106272000,
+  37: 114996000,
+  38: 124184000,
+  39: 133848000,
+  40: 144000000,
+  41: 154652000,
+  42: 165816000,
+  43: 177504000,
+  44: 189728000,
+  45: 202500000,
+  46: 215832000,
+  47: 229736000,
+  48: 244224000,
+  49: 259308000,
+  50: 275000000,
+};
+
+export const initialPrestigeMultCosts: PrestigeMultipliersWithValue = [
+  [5, { qty: 0, cost: 5, multPerPrest: 1 }],
+  [15, { qty: 0, cost: 50, multPerPrest: 15 / 50 }],
+  [30, { qty: 0, cost: 250, multPerPrest: 30 / 250 }],
+  [50, { qty: 0, cost: 750, multPerPrest: 50 / 750 }],
+  [100, { qty: 0, cost: 2500, multPerPrest: 100 / 2500 }],
+];
+
+export const multiplierIndex: Record<PrestigeMultiplier, 0 | 1 | 2 | 3 | 4> = {
+  5: 0,
+  15: 1,
+  30: 2,
+  50: 3,
+  100: 4,
+};
+
+export type PrestigeMultiplierCosts<
+  M extends PrestigeMultiplier = PrestigeMultiplier
+> = [
+  multiplier: M,
+  values: { qty: number; cost: PrestigePoints; multPerPrest: Multiplier }
+];
+
+export type PrestigeMultipliersWithValue = [
+  PrestigeMultiplierCosts<5>,
+  PrestigeMultiplierCosts<15>,
+  PrestigeMultiplierCosts<30>,
+  PrestigeMultiplierCosts<50>,
+  PrestigeMultiplierCosts<100>
+];
+
+export type PrestigeMultiplier = 5 | 15 | 30 | 50 | 100;
+
+export type PrestigePoints = number;
+
+export type Dollars = number;
+
+export type Multiplier = number;
+
+export type PrestigeLevel = typeof prestigeLevels[number];
